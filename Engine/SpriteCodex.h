8b7b629efad98f63fb531024287d53a995f7047a
SamLProgrammer <samuel.lopez@uptc.edu.co>
2025-03-26 10:24:10 -0500

    Adjusting SpriteCodex to Own version

--- Engine/SpriteCodex.h
+++ Engine/SpriteCodex.h
@@ -6,14 +6,35 @@
 class SpriteCodex
 {
 public:
-	// centered drawing of 14x14 sprite
-	static void DrawBall(const Vec2D& center, Graphics& gfx);
-	// centered drawing of 200x160 sprite
-	static void DrawTitle(const Vec2D& center, Graphics& gfx);
-	// centered drawing of 84x64 sprite
-	static void SpriteCodex::DrawGameOver(const Vec2D& center, Graphics& gfx);
-	// centered drawing of 104x19 sprite
-	static void SpriteCodex::DrawReady(const Vec2D& center, Graphics& gfx);
-	// centered drawing of 24x24 sprite
-	static void SpriteCodex::DrawPoo(const Vec2D& center, Graphics& gfx);
+	// width and height of all tiles
+	static constexpr int tileSize = 16;
+	// base color for all tiles
+	static constexpr Color baseColor = { 192,192,192 };
+	// 16x16 tile sprites assume (192,192,192) background and top left origin
+	static void DrawTile0(const Vec2D& pos, Graphics& gfx);
+	static void DrawTile1(const Vec2D& pos, Graphics& gfx);
+	static void DrawTile2(const Vec2D& pos, Graphics& gfx);
+	static void DrawTile3(const Vec2D& pos, Graphics& gfx);
+	static void DrawTile4(const Vec2D& pos, Graphics& gfx);
+	static void DrawTile5(const Vec2D& pos, Graphics& gfx);
+	static void DrawTile6(const Vec2D& pos, Graphics& gfx);
+	static void DrawTile7(const Vec2D& pos, Graphics& gfx);
+	static void DrawTile8(const Vec2D& pos, Graphics& gfx);
+	static void DrawTileButton(const Vec2D& pos, Graphics& gfx);
+	static void DrawTileCross(const Vec2D& pos, Graphics& gfx);
+	static void DrawTileFlag(const Vec2D& pos, Graphics& gfx);
+	static void DrawTileBomb(const Vec2D& pos, Graphics& gfx);
+	static void DrawTileBombRed(const Vec2D& pos, Graphics& gfx);
+	// Tile selector function valid input 0-8
+	static void DrawTileNumber(const Vec2D& pos, int n, Graphics& gfx);
+	// Win Screen 254x192 center origin
+	static void DrawWin(const Vec2D& pos, Graphics& gfx);
+
+	// Text for size selection (center origin)
+	//
+	static constexpr int sizeselWidth = 240;
+	static constexpr int sizeselHeight = 56;
+	static void DrawSmall(const Vec2D& pos, Graphics& gfx);
+	static void DrawMedium(const Vec2D& pos, Graphics& gfx);
+	static void DrawLarge(const Vec2D& pos, Graphics& gfx);
 };
